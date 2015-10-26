ac5ea9073603eca04125b89d90f9af8d583bbca1
Stefan Sinapov <stefan.sinapov@gmail.com>
2015-10-26 17:02:34 +0200

    blank project

---
+++ angular/visual studio tasks/VisualStudio2015Task/EmptyWebApplication/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/// <binding BeforeBuild='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var paths = {
+    src: "./Assets/**/*.js",
+    dest: "./wwwroot/js/"
+}
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
