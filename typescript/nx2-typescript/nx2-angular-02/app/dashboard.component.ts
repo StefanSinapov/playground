d69eba5c1853ecbacf999cdaa3d9955fb20d0b84
Stefan Sinapov <stefan.sinapov@negometrix.bg>
2016-03-23 14:14:08 +0200

    heroes angular2

---
+++ typescript/nx2-typescript/nx2-angular-02/app/dashboard.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from 'angular2/core'
+import { Router } from 'angular2/router';
+import { HeroService } from './hero.service'
+import { IHero } from './hero'
+
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: 'app/dashboard.component.html',
+    styleUrls: ['app/dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+    heroes: IHero[];
+
+    constructor(private _heroService: HeroService, private _router: Router) { }
+
+    ngOnInit(): any {
+        this.loadHeroes();
+    }
+
+    loadHeroes(): void {
+        this._heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
+    gotoDetail(hero: IHero) {
+        let link = ['HeroDetail', { id: hero.id }];
+        this._router.navigate(link);
+    }
+}
---
+++ typescript/nx2-typescript/nx2-angular-02/app/hero.service.ts
@@ -0,0 +1,23 @@
+﻿import { IHero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from 'angular2/core';
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    // See the "Take it slow" appendix
+    getHeroesSlowly() {
+        return new Promise<IHero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+
+    getHero(id: number) {
+        return Promise.resolve(HEROES).then(
+            heroes => heroes.filter(hero => hero.id === id)[0]
+        );
+    }
+}
