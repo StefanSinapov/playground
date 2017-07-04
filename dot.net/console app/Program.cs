8dd44fa4fe204863c6e37f6f22cb03c0bf657427
Stefan Sinapov <stefan.sinapov@gmail.com>
2017-07-04 08:50:40 +0300

    dotnet console app

---
+++ dot.net/console app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
